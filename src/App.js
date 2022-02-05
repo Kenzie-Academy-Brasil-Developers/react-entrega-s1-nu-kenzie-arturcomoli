af8b78bf8251fec61502c5afa9530f0ae01f5139
Artur Comoli <artur.comoli@gmail.com>
2022-02-05 16:21:14 -0300

    feat(style): scrollbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import InitialPage from "./components/InitialPage";
 function App() {
   const [listTransactions, setListTransactions] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [listaFiltrada, setListaFiltrada] = useState([])
 
   const handleLogin = () => {
     setIsLoggedIn(!isLoggedIn);
@@ -16,8 +17,6 @@ function App() {
 
   const deletar = (itemDeletar) => {
     const encontrar = listTransactions.find((item) => {
-      console.log(item);
-      console.log(itemDeletar);
       return item.maxId === itemDeletar;
     });
     const indice = listTransactions.indexOf(encontrar);
@@ -25,6 +24,20 @@ function App() {
     arr.splice(indice, 1);
     setListTransactions(arr);
   };
+
+  const filtrarTodos = () => {
+    setListTransactions(listTransactions);
+  };
+
+  const filtrar = (tipo) => {
+    const filtrados = listTransactions.filter((item) => {
+      console.log(item.tipo);
+      console.log(tipo);
+      return item.tipo === tipo;
+    });
+    setListTransactions(filtrados);
+  };
+  console.log(listTransactions);
 
   return (
     <div className="App">
@@ -45,25 +58,12 @@ function App() {
               listTransactions={listTransactions}
               setListTransactions={setListTransactions}
               deletar={deletar}
+              filtrar={filtrar}
+              filtrarTodos={filtrarTodos}
             />
           </main>
         </>
       )}
-      {/* <Header handleLogin={handleLogin}/>
-      <main>
-        <section>
-          <Form
-            listTransactions={listTransactions}
-            setListTransactions={setListTransactions}
-          />
-          <TotalMoney listTransactions={listTransactions} />
-        </section>
-        <List
-          listTransactions={listTransactions}
-          setListTransactions={setListTransactions}
-          deletar={deletar}
-        />
-      </main> */}
     </div>
   );
 }
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,33 +3,39 @@ import Button from "../Button";
 import Card from "../Card";
 import Vazio from "./Vazio.png";
 // import { useState } from "react";
-const List = ({ listTransactions, setListTransactions, deletar }) => {
-  // const [maxId, setMaxId] = useState(
-  //   listTransactions.reduce((prev, cur) => (prev.id > cur.id ? prev : cur), 0)
-  // );
-  // console.log(maxId);
-
-  // const gerarId = () => {
-  //   console.log("oi");
-  //   setMaxId(maxId + 1);
-  //   return maxId;
-  // };
-
+const List = ({
+  listTransactions,
+  setListTransactions,
+  deletar,
+  filtrar,
+  filtrarTodos,
+}) => {
   return (
     <aside>
       <div className="aside-flex">
         <h3>Resumo financeiro</h3>
         <div>
-          <Button children={"Todos"} className={"list-button"} />
-          <Button children={"Entradas"} className={"list-button"} />
-          <Button children={"Despesas"} className={"list-button"} />
+          <Button
+            children={"Todos"}
+            className={"list-button"}
+            onClick={() => filtrarTodos}
+          />
+          <Button
+            children={"Entradas"}
+            className={"list-button"}
+            onClick={() => filtrar("Entrada")}
+          />
+          <Button
+            children={"Despesas"}
+            className={"list-button"}
+            onClick={() => filtrar("Despesa")}
+          />
         </div>
       </div>
       <ul>
         {listTransactions.length === 0 ? (
           <div className="vazio">
             <p>Não existem lançamentos ainda!!</p>
-            <img src={Vazio} alt="#" />
             <img src={Vazio} alt="#" />
             <img src={Vazio} alt="#" />
           </div>
@@ -46,8 +52,6 @@ const List = ({ listTransactions, setListTransactions, deletar }) => {
               setListTransactions={setListTransactions}
               deletar={deletar}
               itemDeletar={transaction.maxId}
-              tipoDeletar={transaction.tipo}
-              valorDeletar={transaction.valor}
             />
           ))
         )}
