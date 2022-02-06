4a39528c60fb56b15af6dd6ea0ffb782ef01b67e
Artur Comoli <artur.comoli@gmail.com>
2022-02-06 16:32:48 -0300

    fix(Totalmoney): now adds for Entrada and subtracts for Despesas

--- src/components/TotalMoney/index.jsx
+++ src/components/TotalMoney/index.jsx
@@ -1,7 +1,11 @@
 import "./styles.css";
 const TotalMoney = ({ listTransactions }) => {
   const total = listTransactions.reduce((acc, actual) => {
-    return acc + Number(actual.valor);
+    if (actual.tipo === "Entrada") {
+      return acc + Number(actual.valor);
+    } else {
+      return acc - Number(actual.valor);
+    }
   }, 0);
 
   const mostrar = total.toFixed(2).replace(".", ",");
